4f842d3d7919dff3f879b26a2725a201faa4a44e
TheTrash <mattia.polticchia@gmail.com>
2023-12-06 22:04:51 +0100

    Easy peasy solution

--- day 6/singlerace.py
+++ day 6/singlerace.py
@@ -7,5 +7,26 @@ time = [ i for i in l[0].split(":")[1].split(" ") if i != ""]
 distance =[ i for i in l[1].split(":")[1].split(" ") if i != ""]
 time = int("".join(time))
 distance = int("".join(distance))
-print(time)
-print(distance)
+print("time", time)
+print("distance", distance)
+
+
+
+
+first = 0
+for i in range(distance//2):
+    if((time-i)*i > distance):
+        print(i)
+        first = i
+        break
+print("mid",distance//2)
+print((time-first)*(first) > distance)
+
+last = 0
+for t in range(first,distance,1):
+    if not ((time-t)*t > distance):
+        print(t)
+        last = t
+        break
+
+print(last - first)
