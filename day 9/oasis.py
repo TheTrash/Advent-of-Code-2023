d10e95fe6f820f99e6015e3afe9837bd5b1dbf3d
TheTrash <mattia.polticchia@gmail.com>
2023-12-10 13:40:34 +0100

    Day 9 solved

---
+++ day 9/oasis.py
@@ -0,0 +1,35 @@
+f = open("input", "r")
+l = [ n.replace("\n", "").split(" ") for n in f.readlines()]
+
+
+res = []
+for curr in range(len(l)):
+
+    passage = [l[curr]]
+    p = 0
+    while(True):
+        passage.append([])
+        for i in range((len(passage[p])-1)):
+            #print(int(passage[p][i]), int(passage[p][i+1]))
+            passage[p+1].append((int(passage[p][i+1]) - int(passage[p][i])))
+        
+        p += 1
+        #print(passage[p])
+        tc = 0
+        for e in range(len(passage[p])):
+            if passage[p][e] == 0:
+                tc+= 1
+        if tc == len(passage[p]):
+            print(passage[p])
+            break
+
+    #print(passage)
+    tot = 0
+    for p in passage:
+        print(len(p))
+        tot += int(p[-1])
+
+    res.append(tot)
+    print(tot)
+
+print(sum(res))
